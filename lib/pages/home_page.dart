0b5530b230617d6779f69ced7a3e304b25713059
Viraj Shah <virajshah177@gmail.com>
2025-08-07 01:08:13 +0530

    last edit lol

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,7 +14,8 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black87,
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Home Page',style: TextStyle(color: Colors.white60),),
+        backgroundColor: Colors.black45,
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -194,23 +195,151 @@ class HomePage extends StatelessWidget {
                       color: Colors.green[200],
                     ),
                     SizedBox(width: 5.0),
-                    Text(
-                      'Writers: ',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.green[200],
-                      ),
-                    ),
-                    Text(
-                      'Jonathan Nolan, Christopher Nolan',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.green[50],
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Writers: ',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.green[200],
+                          ),
+                        ),
+                        Text(
+                          'Jonathan Nolan, Christopher Nolan',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.green[50],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-
+                SizedBox(height: 8,),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black45,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text('When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.language,
+                      size: 25,
+                      color: Colors.green[200],
+                    ),
+                    SizedBox(width: 8,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Language:',
+                          style: TextStyle(
+                            color: Colors.green[200],
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          'English',
+                          style: TextStyle(
+                            color: Colors.green[50],
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 100,),
+                    Icon(
+                      Icons.map_rounded,
+                      size: 25,
+                      color: Colors.green[200],
+                    ),
+                    SizedBox(width: 8,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          Text(
+                            'Country: ',
+                            style: TextStyle(
+                              color: Colors.green[200],
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            'United States',
+                            style: TextStyle(
+                              color: Colors.green[50],
+                              fontSize: 15,
+                            ),
+                          ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 8,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_rate_outlined,
+                      size: 25,
+                      color: Colors.green[200],
+                    ),
+                    SizedBox(width: 8,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Awards:',
+                          style: TextStyle(
+                          color: Colors.green[100],
+                          fontSize: 15,
+                        ),
+                        ),
+                        Text(
+                          'Won two Oscars',
+                          style: TextStyle(
+                            color: Colors.green[50],
+                            fontSize: 15,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green[300],
+                            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text(
+                            'Shuffle',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
